6221ed45c60309524eb17b5eaff4b15585b73022
Nazar Revutskyy <nrevutskyy@gmail.com>
2021-01-28 13:14:22 +0200

    Update media path

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
                     loader: 'file-loader',
                     options: {
                         name: '[name].[ext]',
-                        outputPath: 'images/'
+                        outputPath: '/customer-portal-styles/images/'
                     }
                 }]
             },
@@ -51,7 +51,7 @@ module.exports = {
                         loader: 'file-loader',
                         options: {
                             name: '[name].[ext]',
-                            outputPath: 'fonts/',
+                            outputPath: '/customer-portal-styles/fonts/',
                             mimetype: 'application/font-woff'
                         }
                     }
