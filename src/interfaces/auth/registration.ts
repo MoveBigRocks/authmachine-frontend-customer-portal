4ebac032f5a7f0b8e8453cc3fcbaa740485d4f74
Nazar Revutskyy <nrevutskyy@gmail.com>
2021-03-23 12:16:44 +0200

    Update registration API method

--- src/interfaces/auth/registration.ts
+++ src/interfaces/auth/registration.ts
@@ -1,6 +1,12 @@
 export interface RegistrationProps {
     isRegister: boolean,
     message: string,
-    register: (values: { username: string, email: string, fullName: string, password: string, }) => void,
+    register: (values: {
+        username: string,
+        email: string,
+        fullName: string,
+        password: string,
+        additionalRegistrationData: any
+    }) => void,
     setPageTitle: (pageTitle: string) => void,
 }
