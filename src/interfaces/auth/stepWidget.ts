03dfbdee3493aaa25dad58ddb61197e457736f50
Nazar <nrevutskyy@gmail.com>
2021-06-01 19:23:28 +0300

    authmachine update

--- src/interfaces/auth/stepWidget.ts
+++ src/interfaces/auth/stepWidget.ts
@@ -1,5 +1,15 @@
-export default interface StepSwitcherProps {
-    first: boolean
-    second: boolean
-    third: boolean
+interface StepSwitcherProps {
+    first: boolean,
+    second: boolean,
+    third: boolean,
+    step: number
 }
+
+interface StepProps {
+    step: number,
+    description: string,
+    valid: boolean,
+    current: boolean
+}
+
+export type {StepSwitcherProps, StepProps};
